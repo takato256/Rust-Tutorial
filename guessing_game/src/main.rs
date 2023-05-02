8a67f7ef89d2bd9844d9e5f58c56902207393c82
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-05-02 17:33:44 +0900

    add explain

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main(){
+    println!("Guess the number");
+
+    println!("Please input your guess");
+
+    /*
+    let apples = 5; <- 不変
+    let mut bananas = 5; <- 可変
+    */
+    let mut guess = String::new();
+
+    /*
+    ioライブラリをインポートしなくても
+    std::io::stdinのように呼び出すことによって利用可
+    */
+    io::stdin()
+        .read_line(&mut guess)  // 標準入力ハンドルのread_lineメソッドを呼び出し
+        .expect("Failed to read line"); // 例外処理
+
+    println!("You guessed: {}", guess);
 }
