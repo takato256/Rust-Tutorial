e4d508ce8b7083ff56711ba3f97eaf9c1e5910cd
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-05-02 17:21:55 +0900

    first commit

---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+
+C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+
+src\main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\guessing_game\target\debug\guessing_game.exe: C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\guessing_game\src\main.rs
