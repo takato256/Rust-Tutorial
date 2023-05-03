a5e032a26dfa3a385e523fb7dfedd714d78c4e7d
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-05-03 22:59:58 +0900

    complete invalid_arrange

---
+++ invalid_arrange/target/debug/deps/invalid_arrange.d
@@ -0,0 +1,5 @@
+C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\invalid_arrange\target\debug\deps\invalid_arrange.exe: src\main.rs
+
+C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\invalid_arrange\target\debug\deps\invalid_arrange.d: src\main.rs
+
+src\main.rs:
---
+++ invalid_arrange/target/debug/invalid_arrange.d
@@ -0,0 +1,1 @@
+C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\invalid_arrange\target\debug\invalid_arrange.exe: C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\invalid_arrange\src\main.rs
