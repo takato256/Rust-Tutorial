ddccaf40e70a10a7bd5086d70ef75837bb777ffd
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-05-03 22:59:22 +0900

    complete types

---
+++ types/target/debug/deps/types.d
@@ -0,0 +1,5 @@
+C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\types\target\debug\deps\types.exe: src\main.rs
+
+C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\types\target\debug\deps\types.d: src\main.rs
+
+src\main.rs:
---
+++ types/target/debug/types.d
@@ -0,0 +1,1 @@
+C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\types\target\debug\types.exe: C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\types\src\main.rs
