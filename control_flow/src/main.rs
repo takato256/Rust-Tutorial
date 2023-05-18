19c72c27e803f8401ec9bdffbea9107d370bf227
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-05-19 08:32:34 +0900

    complete control-flow

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -19,6 +19,7 @@ fn main() {
 }
 */
 
+/*
 fn main() {
     let mut count = 0;
     'counting_up: loop{     // ループラベルを使用することで、breakやcontinueは適用されるループを指定することができる
@@ -40,4 +41,35 @@ fn main() {
     }
     println!("End count = {}", count);
 }
+*/
+/*
+fn main() {
+    let mut number = 3;
 
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+}
+*/
+
+/*
+fn main() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a{
+        println!("the value is: {}", element);
+    }
+}
+*/
+
+fn main(){
+    // revメソッドで中身を逆順に取り出す
+    for number in (1..4).rev(){
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
