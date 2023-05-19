55fded85c570df4da06a882f14a4df5632d37ec1
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-05-19 11:25:31 +0900

    add ownership

---
+++ ownership/target/debug/deps/ownership.d
@@ -0,0 +1,5 @@
+C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\ownership\target\debug\deps\ownership.exe: src\main.rs
+
+C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\ownership\target\debug\deps\ownership.d: src\main.rs
+
+src\main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\ownership\target\debug\ownership.exe: C:\Users\nakaya39\Desktop\folder\Rust-Tutorial\Rust-Tutorial\ownership\src\main.rs
